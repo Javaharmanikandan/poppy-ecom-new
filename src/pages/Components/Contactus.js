fd45365a3ec5b1ca0fae7d0b98870866efbbcc17
Javaharmanikandan <118499371+Javaharmanikandan@users.noreply.github.com>
2023-06-10 00:23:43 +0530

    push

--- src/pages/Components/Contactus.js
+++ src/pages/Components/Contactus.js
@@ -7,7 +7,7 @@ function Contactus() {
     <div class="content_contact">
         <div><p style={{textAlign:"center"}}>Call Our Sleep Experts to <br />Choose your Right Mattress</p></div>
         <div className="Cont-img"><img src="/assests/images/svg/customer.svg" width={50} height={50}/>
-        <a href="tel:+6199942413" style={{color:"white",marginLeft:10}}><p>+91 9054848481</p></a></div>
+        <a href="tel:+91 9054848481" style={{color:"white",marginLeft:10}}><p>+91 9054848481</p></a></div>
     </div>
 </div>
 
