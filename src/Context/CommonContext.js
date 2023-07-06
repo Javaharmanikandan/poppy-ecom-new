fe5050a5c61393dfdcc409d3cda8530e2c35d03a
@DevaPandiyan <58162262+Rabinswalterraj@users.noreply.github.com>
2023-07-06 10:26:54 +0530

    changes

--- src/Context/CommonContext.js
+++ src/Context/CommonContext.js
@@ -2,7 +2,7 @@ import React, { useState, createContext } from "react";
 export const CommonContext = createContext(null);
 
 export const CommonContextProvider = ({ children }) => {
-    const [cartModal, setCartModal] = useState(true);
+    const [cartModal, setCartModal] = useState(false);
 
     const value ={
        cartModal,
